67d582ef68f8c4b0e23e2617e1f55ceafd101f02
amolmt <tangadeamol@gmail.com>
2018-02-25 21:02:33 +0530

    Created database schema

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post', 'PostController@post');
+Route::get('/profile', 'ProfileController@profile');
+Route::get('/category', 'CategoryController@category');
+
+
